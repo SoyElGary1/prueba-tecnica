ea63a63410bf92e6757e1b90951f80d8b5bcb407
Christopher Nunez <elgary1129@gmail.com>
2024-04-10 23:09:38 -0400

    Prueba tecnica funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { useState } from "react";
+import "./App.css";
+
+//const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${threeFirsttWords}`
+const CAT_ENDPOINT_RANDOM_FACT = `https://catfact.ninja/fact`;
+const CAT_PREFIX_IMAGE_URL = `https://cataas.com/cat/`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fact, setFact] = useState();
+  const [imageUrl, setImageUrl] = useState();
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((res) => res.json())
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+      });
+  }, []);
+
+  useEffect(() => {
+
+    if(!fact) return
+
+    const threeFirstWords = fact.split(" ", 3).join(" ");
+    console.log(threeFirstWords);
+
+    fetch(`https://cataas.com/cat/cute/says/${threeFirstWords}?json=true`)
+      .then((res) => res.json())
+      .then((data) => {
+        const { _id } = data;
+        setImageUrl(
+          `${_id}/says/${threeFirstWords}?fontSize=50&fontColor=white`
+        );
+      });
+  }, [fact]);
+
+  // fetch con async await
+  // useEffect(()=>{
+  //   async function getFunfact() {
+  //     const res = await fetch(CAT_ENDPOINT_RANDOM_FACT)
+  //     const json = await res.json()
+  //     setFact(json.fact)
+  //   }
+
+  //   getFunfact()
+  //},[])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <h1>App de gatitos</h1>
+      <section>
+        {fact && <p>{fact}</p>}
+        {imageUrl && (
+          <img
+            src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`}
+            alt={`Image extracted using the three first words for ${fact}`}
+          />
+        )}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
